62ab25a2095f700f49403065835bcb6cb6881448
nakashivu1 <55726583+nakashivu1@users.noreply.github.com>
2021-07-10 01:17:11 -0400

    switch to retropilot and enable logger

--- selfdrive/ui/qt/widgets/setup.cc
+++ selfdrive/ui/qt/widgets/setup.cc
@@ -29,9 +29,19 @@ void PairingQRWidget::showEvent(QShowEvent *event) {
   refresh();
 }
 
-void PairingQRWidget::refresh() {
+void PairingQRWidget::refresh(){
+  Params params;
+  QString IMEI = QString::fromStdString(params.get("IMEI"));
+  QString serial = QString::fromStdString(params.get("HardwareSerial"));
+
+  if (std::min(IMEI.length(), serial.length()) <= 5) {
+    qrCode->setText("Error getting serial: contact support");
+    qrCode->setWordWrap(true);
+    qrCode->setStyleSheet(R"(font-size: 60px;)");
+    return;
+  }
   QString pairToken = CommaApi::create_jwt({{"pair", true}});
-  QString qrString = "https://my.comma.ai/?pair=" + pairToken;
+  QString qrString = IMEI + "--" + serial + "--" + pairToken;
   this->updateQrCode(qrString);
 }
 
@@ -89,12 +99,12 @@ PrimeUserWidget::PrimeUserWidget(QWidget* parent) : QWidget(parent) {
   )");
 
   // set up API requests
-  /*std::string dongleId = Params().get("DongleId");
+  std::string dongleId = Params().get("DongleId");
   if (util::is_valid_dongle_id(dongleId)) {
-    std::string url = "https://api.commadotai.com/v1/devices/" + dongleId + "/owner";
+    std::string url = "https://api.retropilot.org/v1.1/devices/" + dongleId + "/owner";
     RequestRepeater *repeater = new RequestRepeater(this, QString::fromStdString(url), "ApiCache_Owner", 6);
     QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &PrimeUserWidget::replyFinished);
-  }*/
+  }
 }
 
 void PrimeUserWidget::replyFinished(const QString &response) {
@@ -218,17 +228,15 @@ SetupWidget::SetupWidget(QWidget* parent) : QFrame(parent) {
   setSizePolicy(sp_retain);
 
   // set up API requests
-  /*std::string dongleId = Params().get("DongleId");
+  std::string dongleId = Params().get("DongleId");
   if (util::is_valid_dongle_id(dongleId)) {
-    std::string url = "https://api.commadotai.com/v1.1/devices/" + dongleId + "/";
+    std::string url = "https://api.retropilot.org/v1.1/devices/" + dongleId + "/";
     RequestRepeater* repeater = new RequestRepeater(this, QString::fromStdString(url), "ApiCache_Device", 5);
 
     QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &SetupWidget::replyFinished);
     QObject::connect(repeater, &RequestRepeater::failedResponse, this, &SetupWidget::parseError);
   }
   hide(); // Only show when first request comes back
-  */
-  parseError("");
 }
 
 void SetupWidget::parseError(const QString &response) {
